d23c714f43aea3ee3f754db5793e23fca06a0742
KenilAvaiyaa <avaiyakekenil@gmail.com>
2024-12-30 14:55:34 -0500

    fixed loader page

--- app/Components/Loader/loader.tsx
+++ app/Components/Loader/loader.tsx
@@ -7,21 +7,21 @@ import { motion } from "framer-motion";
 const loader = () => {
   useGSAP(() => {
     const time = gsap.timeline({});
-    time.from(
+    time.to(
       ".line_one .sholk span",
       {
-        y: 100,
+        y:-100,
         duration: 2,
         ease: "cubic-bezier(0.45, 0, 0.55, 1)",
-        delay: 0.5,
+        delay: 1,
         stagger: 0.5,
       },
       "maintext"
     );
-    time.from(
+    time.to(
       ".line_two .sholk span",
       {
-        y: 80,
+        y: -100,
         duration: 2,
         ease: "cubic-bezier(0.45, 0, 0.55, 1)",
         delay: 3,
@@ -46,8 +46,8 @@ const loader = () => {
     <>
       <div className="loader w-full h-screen fixed z-[999] top-0 bg-[#F5EFE7] flex justify-center items-center">
         <div className="loder_text flex gap-1 flex-col px-4 sm:px-6 md:px-8">
-          <div className="line_one h-[11vh] relative overflow-hidden flex justify-center items-center">
-            <h1 className="sholk font-[pandit_text] text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-black text-[#D7492B] flex flex-wrap justify-center gap-2 sm:gap-3">
+          <div className="line_one h-[11vh] relative overflow-hidden  flex justify-center items-center">
+            <h1 className="sholk font-[pandit_text] text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-black text-[#D7492B] flex flex-wrap justify-center gap-2 sm:gap-3" style={{transform: "translateY(100px)"}}>
               <span>वक्रतुण्ड</span>
               <span>महाकाय</span>
               <span>सूर्यकोटि</span>
@@ -55,7 +55,7 @@ const loader = () => {
             </h1>
           </div>
           <div className="line_two h-fit lg:h-[10vh] relative overflow-hidden flex justify-center items-center">
-            <h1 className="sholk font-[pandit_text] text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-black text-[#D7492B] flex flex-wrap justify-center gap-2 sm:gap-3">
+            <h1 className="sholk font-[pandit_text] text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-black text-[#D7492B] flex flex-wrap justify-center gap-2 sm:gap-3" style={{transform: "translateY(100px)"}}>
               <span>निर्विघ्नं</span>
               <span>कुरु</span>
               <span>मे</span>
